d186462fa1a8810382242210ebf87695eaccfd56
PRONOY <pronoybanik82@gmail.com>
2023-03-01 00:03:04 +0600

    user delete id add

--- index.js
+++ index.js
@@ -136,6 +136,13 @@ async function run() {
 
     })
 
+    app.delete('/user/:id', verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) }
+      const result = await usersCollection.deleteOne(filter)
+      res.send(result)
+    });
+
     app.get('/users/admin/:email', async (req, res) => {
       const email = req.params.email;
       const query = { email };
